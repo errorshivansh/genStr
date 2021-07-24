c786aae9c8fe6ed7f20d75efd457a8f48fdcb080
SHIVANSH <75574324+errorshivansh@users.noreply.github.com>
2021-07-24 13:45:44 +0530

    Update genStr.py

--- genStr.py
+++ genStr.py
@@ -1,9 +1,6 @@
-import os
-import json
-import time
 import asyncio
 
-from bot import Bot, Config
+from bot import bot, HU_APP
 from pyromod import listen
 from asyncio.exceptions import TimeoutError
 
@@ -15,21 +12,19 @@ from pyrogram.errors import (
     PhoneCodeInvalid, PhoneCodeExpired
 )
 
-bot = Bot()
-
-API_TEXT = """Hi {}
-Welcome to Pyrogram's `HU_STRING_SESSION` generator Bot.
-`Send your API_ID to Continue.`"""
-HASH_TEXT = "`Send your API_HASH to Continue.`\n\nPress /cancel to Cancel."
+API_TEXT = """Hi, {}.
+I am String Session Generator Bot. I will generate String Session of your Telegram Account.
+By @Errorshivansh
+Now send your `API_ID` same as `APP_ID` to Start Generating Session.\n You can Support On @EivaSupport join Please """
+HASH_TEXT = "Now send your `API_HASH`.\n\nPress /cancel to Cancel Task."
 PHONE_NUMBER_TEXT = (
-    "`Now send your Phone number to Continue"
-    " include Country code. eg. +13124562345`\n\n"
-    "Press /cancel to Cancel."
+    "Now send your Telegram account's Phone number in International Format. \n"
+    "Including Country code. Example: **+14154566376**\n\n"
+    "Press /cancel to Cancel Task."
 )
 
-
 @bot.on_message(filters.private & filters.command("start"))
-async def genStr(bot: Bot, msg: Message):
+async def genStr(_, msg: Message):
     chat = msg.chat
     api = await bot.ask(
         chat.id, API_TEXT.format(msg.from_user.mention)
@@ -37,141 +32,142 @@ async def genStr(bot: Bot, msg: Message):
     if await is_cancel(msg, api.text):
         return
     try:
-        int(api.text)
+        check_api = int(api.text)
     except Exception:
-        await api.delete()
-        await msg.reply("`API ID Invalid.`\nPress /start to create again.")
+        await msg.reply("`API_ID` is Invalid.\nPress /start to Start again.")
         return
     api_id = api.text
-    await api.delete()
     hash = await bot.ask(chat.id, HASH_TEXT)
     if await is_cancel(msg, hash.text):
         return
+    if not len(hash.text) >= 30:
+        await msg.reply("`API_HASH` is Invalid.\nPress /start to Start again.")
+        return
     api_hash = hash.text
-    await hash.delete()
+    while True:
+        number = await bot.ask(chat.id, PHONE_NUMBER_TEXT)
+        if not number.text:
+            continue
+        if await is_cancel(msg, number.text):
+            return
+        phone = number.text
+        confirm = await bot.ask(chat.id, f'`Is "{phone}" correct? (y/n):` \n\nSend: `y` (If Yes)\nSend: `n` (If No)')
+        if await is_cancel(msg, confirm.text):
+            return
+        if "y" in confirm.text:
+            break
     try:
         client = Client("my_account", api_id=api_id, api_hash=api_hash)
     except Exception as e:
-        await bot.send_message(chat.id ,f"**ERROR:** `{str(e)}`\nPress /start to create again.")
+        await bot.send_message(chat.id ,f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
         return
     try:
         await client.connect()
     except ConnectionError:
         await client.disconnect()
         await client.connect()
-    await msg.reply("`Successfully Connected to you Client.`")
-    while True:
-        number = await bot.ask(chat.id, PHONE_NUMBER_TEXT)
-        if not number.text:
-            continue
-        if await is_cancel(msg, number.text):
-            await client.disconnect()
-            return
-        phone = number.text
-        await number.delete()
-        confirm = await bot.ask(chat.id, f'`Is "{phone}" correct? (y/n):` \n\ntype: `y` (If Yes)\ntype: `n` (If No)')
-        if await is_cancel(msg, confirm.text):
-            await client.disconnect()
-            return
-        if "y" in confirm.text.lower():
-            await confirm.delete()
-            break
     try:
         code = await client.send_code(phone)
         await asyncio.sleep(1)
     except FloodWait as e:
-        await msg.reply(f"`you have floodwait of {e.x} Seconds`")
-        return await bot.sleep(msg)
+        await msg.reply(f"You have Floodwait of {e.x} Seconds")
+        return
     except ApiIdInvalid:
-        await msg.reply("`Api Id and Api Hash are Invalid.`\n\nPress /start to create again.")
-        return await bot.sleep(msg)
+        await msg.reply("API ID and API Hash are Invalid.\n\nPress /start to Start again.")
+        return
     except PhoneNumberInvalid:
-        await msg.reply("`your Phone Number is Invalid.`\n\nPress /start to create again.")
-        return await bot.sleep(msg)
+        await msg.reply("Your Phone Number is Invalid.\n\nPress /start to Start again.")
+        return
     try:
         otp = await bot.ask(
-            chat.id, ("`An otp is sent to your phone number, "
-                      "Please enter otp in\n`1 2 3 4 5` format.`\n\n"
-                      "`If Bot not sending OTP then try` /restart `cmd and again` /start `the Bot.`\n"
+            chat.id, ("An OTP is sent to your phone number, "
+                      "Please enter OTP in `1 2 3 4 5` format. __(Space between each numbers!)__ \n\n"
+                      "If Bot not sending OTP then try /restart and Start Task again with /start command to Bot.\n"
                       "Press /cancel to Cancel."), timeout=300)
+
     except TimeoutError:
-        await msg.reply("`Time limit reached of 5 min.\nPress /start to create again.`")
-        return await bot.sleep(msg)
+        await msg.reply("Time limit reached of 5 min.\nPress /start to Start again.")
+        return
     if await is_cancel(msg, otp.text):
-        return await client.disconnect()
+        return
     otp_code = otp.text
-    await otp.delete()
     try:
         await client.sign_in(phone, code.phone_code_hash, phone_code=' '.join(str(otp_code)))
     except PhoneCodeInvalid:
-        await msg.reply("`Invalid Code.`\n\nPress /start to create again.")
-        return await bot.sleep(msg)
+        await msg.reply("Invalid Code.\n\nPress /start to Start again.")
+        return
     except PhoneCodeExpired:
-        await msg.reply("`Code is Expired.`\n\nPress /start to create again.")
-        return await bot.sleep(msg)
+        await msg.reply("Code is Expired.\n\nPress /start to Start again.")
+        return
     except SessionPasswordNeeded:
         try:
             two_step_code = await bot.ask(
                 chat.id, 
-                "`This account have two-step verification code.\nPlease enter your second factor authentication code.`\nPress /cancel to Cancel.",
+                "Your account have Two-Step Verification.\nPlease enter your Password.\n\nPress /cancel to Cancel.",
                 timeout=300
             )
         except TimeoutError:
-            await msg.reply("`Time limit reached of 5 min.\n\nPress /start to create again.`")
-            return await bot.sleep(msg)
+            await msg.reply("`Time limit reached of 5 min.\n\nPress /start to Start again.`")
+            return
         if await is_cancel(msg, two_step_code.text):
-            return await client.disconnect()
+            return
         new_code = two_step_code.text
-        await two_step_code.delete()
         try:
             await client.check_password(new_code)
         except Exception as e:
             await msg.reply(f"**ERROR:** `{str(e)}`")
-            return await bot.sleep(msg)
+            return
     except Exception as e:
         await bot.send_message(chat.id ,f"**ERROR:** `{str(e)}`")
-        return await bot.sleep(msg)
-    session_string = await client.export_session_string()
-    await client.send_message("me", f"#PYROGRAM #HU_STRING_SESSION\n\n```{session_string}```")
-
-    text = "`String Session is Successfully Generated.\nClick on Button Below.`"
-    reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton(text="Click Me", url=f"tg://openmessage?user_id={chat.id}")]]
-    )
-    await bot.send_message(chat.id, text, reply_markup=reply_markup)
-    return await bot.sleep(msg)
+        return
+    try:
+        session_string = await client.export_session_string()
+        await client.send_message("me", f"#STRING_SESSION of your account \n\n```{session_string}``` \n\nA Bot By @errorshivansh")
+        await client.disconnect()
+        text = "String Session is Successfully Generated.\nClick on Below Button."
+        reply_markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="Show String Session", url=f"tg://openmessage?user_id={chat.id}")]]
+        )
+        await bot.send_message(chat.id, text, reply_markup=reply_markup)
+    except Exception as e:
+        await bot.send_message(chat.id ,f"**ERROR:** `{str(e)}`")
+        return
 
 
 @bot.on_message(filters.private & filters.command("restart"))
-async def restart(bot: Bot, msg: Message):
-    if msg.from_user.id == 1320929227:
-        await msg.reply('✅')
-        return Config.HU_APP.restart()
+async def restart(_, msg: Message):
+    await msg.reply("Restarted Bot!")
+    HU_APP.restart()
 
 
 @bot.on_message(filters.private & filters.command("help"))
-async def start(_, msg: Message):
+async def restart(_, msg: Message):
     out = f"""
-Hello {msg.from_user.mention}, this is Pyrogram Session String Generator Bot \
-which gives you `HU_STRING_SESSION` for your UserBot.
-It needs `API_ID` , `API_HASH` , `PHONE_NUMBER` and `One time Verification Code` \
-which will send to your `PHONE_NUMBER`.
-you have to put `OTP` in `1 2 3 4 5` this format.
-**NOTE:** `If bot not Sending Otp to your PHONE_NUMBER then try` 
-/restart `Command and again` /start `your Process.`
-(C) Author: [Shivansh](https://t.me/Royal_king7) and \
-[Team Eiva](https://t.me/TheEiva)
-Give a Star ⭐️ to [REPO](https://github.com/errorshivansh/TG-session-string) if you like this Bot.
+Hi, {msg.from_user.mention}. This is Pyrogram Session String Generator Bot. \
+I will give you `STRING_SESSION` for your UserBot.
+It needs `API_ID`, `API_HASH`, Phone Number and One Time Verification Code. \
+Which will be sent to your Phone Number.
+You have to put **OTP** in `1 2 3 4 5` this format. __(Space between each numbers!)__
+**NOTE:** If bot not Sending OTP to your Phone Number than send /restart Command and again send /start to Start your Process. 
+Must Join Channel for Bot Updates !!
 """
-    await msg.reply(out, disable_web_page_preview=True)
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton('Support Channel', url='https://t.me/TheEiva'),
+                InlineKeyboardButton('Developer', url='https://t.me/Eivasupport')
+            ],
+           
+
+    )
+    await msg.reply(out, reply_markup=reply_markup)
 
 
 async def is_cancel(msg: Message, text: str):
     if text.startswith("/cancel"):
-        await msg.reply("`Process Cancelled.`")
+        await msg.reply("Process Cancelled.")
         return True
     return False
-
 
 if __name__ == "__main__":
     bot.run()
